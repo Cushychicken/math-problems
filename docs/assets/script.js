4f8b7c520a2d40e5aef60bc56b5f849f9b71303c
Nash Reilly <cushychicken@gmail.com>
2023-12-30 13:50:15 -0500

    Working PDF export of worksheets

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -41,3 +41,53 @@ function displayProblems() {
 // Initialize with default values on page load
 document.addEventListener('DOMContentLoaded', displayProblems);
 
+function tableToPDF() {
+    const pdf = new jspdf.jsPDF(); // Default to portrait orientation
+
+    // Get the original table and clone it
+    const originalTable = document.getElementById('problems');
+    const clonedTable = originalTable.cloneNode(true);
+
+    // Remove the "Action" column from each row in the cloned table
+    Array.from(clonedTable.rows).forEach(row => {
+        row.deleteCell(-1); // Assuming the "Action" column is the last one
+    });
+
+    // Header, Footer, and Name Line Text
+    const headerFooterText = "Create worksheets like this for free at www.math-problems.com!";
+    const nameLine = "Name: __________________________________________________";
+    const nameLinePositionY = 25; // Vertical position for the name line
+    const tableStartPositionY = 50; // Adjusted start position for the table
+
+    let isFirstPage = true; // Flag to check if it's the first page
+
+    // Generate PDF with header, footer, and name line on the first page
+    pdf.autoTable({
+        html: clonedTable,
+        startY: tableStartPositionY, // Start the table below the name line
+        didDrawPage: function(data) {
+            // Settings for header, footer, and name line
+            pdf.setFontSize(10);
+            pdf.setTextColor(40);
+            const pageWidth = pdf.internal.pageSize.width;
+            const margin = 10; // Define the margin for right alignment
+            const headerFooterTxtWidth = pdf.getStringUnitWidth(headerFooterText) * pdf.internal.getFontSize() / pdf.internal.scaleFactor;
+            const headerFooterX = (pageWidth - headerFooterTxtWidth) / 2; // Center alignment for header/footer
+
+            // Header
+            pdf.text(headerFooterText, headerFooterX, 10);
+
+            // Name Line on the first page only
+            if (isFirstPage) {
+                const nameLineX = pageWidth - margin; // Right alignment for name line
+                pdf.text(nameLine, nameLineX, nameLinePositionY, { align: 'right' }); // Right-aligned name line
+                isFirstPage = false; // Set the flag to false after drawing on the first page
+            }
+
+            // Footer
+            pdf.text(headerFooterText, headerFooterX, pdf.internal.pageSize.height - 10);
+        }
+    });
+
+    pdf.save('math-problems.pdf');
+}
